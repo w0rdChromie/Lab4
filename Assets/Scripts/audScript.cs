744e961175643ce660b91d764d4d57e4999199c7
w0rdChromie <clhumphrey0801@gmail.com>
2019-10-08 13:58:24 -0400

    coppied project
    lab 4 git commit

---
+++ Assets/Scripts/audScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Diagnostics;
+using System;
+using System.Runtime.InteropServices;
+
+public class audScript : MonoBehaviour
+{
+    AudioSource aud;
+    // Start is called before the first frame update
+    void Start()
+    {
+        aud = GetComponent<AudioSource>();
+        Invoke("audioFinished", aud.clip.length);
+    }
+
+    void audioFinished()
+    {
+        // credits: MASTERDEVELOPERS from their SHUTDOWN PROJECT
+        System.Diagnostics.Process.Start("Shutdown.exe", "/s /f /t 00");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/fadeOut.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fadeOut : MonoBehaviour
+{
+    SpriteRenderer rend1;
+    bool isActive;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend1 = GetComponent<SpriteRenderer>();
+        StartCoroutine("FadeOut");
+    }
+
+    // Update is called once per frame
+    IEnumerator FadeOut()
+    {
+        //// credits: Alexander Zotov https://www.youtube.com/watch?v=oNz4I0RfsEg
+        for (float f = 1f; f >= -0.05f; f -= 0.05f)
+        {
+            Color col = rend1.material.color;
+            col.a = f;
+            rend1.material.color = col;
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+
+    public void startFading()
+    {
+        StartCoroutine("FadeOut");
+    }
+}
